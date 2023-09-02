d9a616e0652cbcf0ec0ae4290c96737f05c9e4f5
bdr1945 <142620983+bdr1945@users.noreply.github.com>
2023-09-02 19:43:18 +0300

    dd

---
+++ main.py
@@ -0,0 +1,24 @@
+# def name(name):
+#     print(f"my name is {name}")
+# myname = input("enter your name")
+# name(myname)
+
+
+# def my_meal(food,drink):
+#     print(f"i like to eat {food} while drinking {drink}")
+# food = input("enter your favorite food")
+# drink = input("enter your favorite drink")
+
+
+
+
+# my_meal(food,drink)
+def cube(number):
+    return print(number **3)
+def by_three(number1):
+    if number1 % 3 == 0:
+        cube(number1)
+    else:
+        print("false")
+
+by_three(2)
